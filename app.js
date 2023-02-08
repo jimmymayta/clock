65abc1ecc95446ee65c438d5fad610a7091dde39
Jimmy Mayta <jimmyymaytaj@gmail.com>
2023-02-08 19:19:41 -0400

    Clock

---
+++ app.js
@@ -0,0 +1,16 @@
+const clock = document.querySelector("#clock");
+
+const horaactual = () => {
+  const date = new Date();
+
+  let hora = date.getHours();
+  let minutos = date.getMinutes();
+  let segundos = date.getSeconds();
+  return `${hora} : ${minutos} : ${segundos}`;
+};
+
+const horamostrar = () => {
+  clock.textContent = horaactual();
+};
+
+setInterval(horamostrar, 100);
